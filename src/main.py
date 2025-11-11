2841bc5d6b0761c8bd964b26cf1b08f15c6b0fb3
assortednuts <korvin@coollink.org>
2025-11-10 21:33:37 -0600

    fully implemented the MC server whitelist system

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.commands import default_permissions
 
 from mcrcon import MCRcon
 
@@ -16,9 +17,26 @@ intents.message_content = True  # This may be useless but i'll leave it for now
 
 bot = commands.Bot(command_prefix='/', intents=intents)
 
+# Minecraft connect settings
+rcon_ip = '127.0.0.1'
+rcon_pass = os.getenv('RCON_PASS')
+
+def run_mc_command(command):
+    with MCRcon(rcon_ip, rcon_pass) as mcr:
+        resp = mcr.command("/"+command)
+        print('Minecraft server replied:', resp)
+        return resp
+
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}')
+
+@bot.before_invoke
+async def ensure_user_in_database(ctx):
+    # Ensure user exists in database
+    if not userbase.user_exists(ctx.author.id):
+        userbase.user_create(ctx.author.id, ctx.author.name)
+        print(f"Created new user {ctx.author.name} with ID {ctx.author.id}")
 
 @bot.event
 async def on_message(ctx):
@@ -32,24 +50,43 @@ async def on_message(ctx):
     await bot.process_commands(ctx)
 
 @bot.slash_command()
-async def linkmc(ctx, minecraft_username):
-    whitelist_add = userbase.add_minecraft_user(ctx.author.id, minecraft_username)
+async def linkmc(ctx, minecraft_username: str, version='auto'):
+    whitelist_add = userbase.add_minecraft_user(ctx.author.id, minecraft_username, version)
 
     if whitelist_add == minecraft_username:
-        with MCRcon("127.0.0.1", os.getenv('RCON_PASS')) as mcr:
-            resp = mcr.command("/whitelist add " + minecraft_username)
-            print(resp)
+        whitelist_resp = run_mc_command("whitelist add " + minecraft_username)
+        if whitelist_resp == f'Added {minecraft_username} to the whitelist':
+            await ctx.respond(f'Added Java user {minecraft_username}')
+        elif whitelist_resp == 'Player is already whitelisted':
+            await ctx.respond(whitelist_resp)
+        else:
+            await ctx.respond('Sorry something went wrong')
+    elif whitelist_add == "." + minecraft_username:
+        run_mc_command(f"fwhitelist add {minecraft_username}")
+        await ctx.respond(f"Added Bedrock user {minecraft_username}")
     elif whitelist_add == 404:
-        await ctx.reply("Java user not found")
+        await ctx.respond("User not found")
     elif whitelist_add == 405:
-        await ctx.reply("Account already linked, use /unlinkmc to link a new account")
+        await ctx.respond("Account already linked, use /unlinkmc to link a new account")
 
 @bot.slash_command()
 async def unlinkmc(ctx):
     whitelist_remove = userbase.remove_minecraft_user(ctx.author.id)
-    if whitelist_remove != None:
-        with MCRcon("127.0.0.1", os.getenv('RCON_PASS')) as mcr:
-            resp = mcr.command("/whitelist remove " + whitelist_remove)
-            print(resp)
+    if whitelist_remove != None and "." not in whitelist_remove:
+        whitelist_remove_response = run_mc_command("whitelist remove " + whitelist_remove)
+        if whitelist_remove_response == f'Removed {whitelist_remove} from the whitelist':
+            await ctx.respond(f'Removed {whitelist_remove} from the whitelist')
+    elif whitelist_remove != None and "." in whitelist_remove:
+        run_mc_command(f"fwhitelist remove {whitelist_remove}")
+        await ctx.respond('Unlinked Bedrock Account')
+    else:
+        await ctx.respond('No account linked')
 
+@bot.slash_command()
+async def mcrun(ctx, command: str):
+    resp = run_mc_command(command)
+    if resp != "":
+        await ctx.respond(resp)
+    else:
+        await ctx.respond("Server did not reply")
 bot.run(os.getenv('DISCORD_TOKEN'))
--- src/userbase.py
+++ src/userbase.py
@@ -4,11 +4,12 @@ import requests
 userDB = sqlite3.connect('basement_userbase.db', isolation_level=None)
 cur = userDB.cursor()
 
-cur.execute("CREATE TABLE userbase(id INTEGER PRIMARY KEY, username TEXT, mc_username TEXT, mc_id TEXT)") # Remove after developement is complete
+cur.execute("CREATE TABLE userbase(id INTEGER PRIMARY KEY, username TEXT, mc_username TEXT, mc_id TEXT, mc_version TEXT)") # Remove after developement is complete
 cur.execute("PRAGMA journal_mode=WAL")
 
 def checkpoint_db():
     cur.execute("PRAGMA wal_checkpoint(FULL)")
+    userDB.commit()
 
 def user_exists(userid):
     cur.execute("SELECT id FROM userbase WHERE id=?", (userid,))
@@ -20,36 +21,54 @@ def user_exists(userid):
         print("User exists")
         return True
 
+def read_info(userid, column):
+    cur.execute("SELECT ? FROM userbase WHERE id=?", (column, userid,))
+    data = cur.fetchall()
+    return data[0][0]
+
 def user_create(userid, username):
     cur.execute("INSERT INTO userbase(id, username) VALUES(?, ?)", (userid, username,))
+    checkpoint_db()
     print(f"Added user {username} with id {userid} to database")
 
-def add_minecraft_user(userid, mcusername):
+def add_minecraft_user(userid, mcusername, version):
     cur.execute("SELECT mc_username FROM userbase WHERE id=?", (userid,))
     data = cur.fetchall()
     print(data)
 
-    cur.execute("UPDATE userbase SET mc_username=? WHERE id=?", (mcusername, userid,)) 
-    # Get Minecraft ID from API
-    mc_id_api_call = requests.get("https://api.mojang.com/users/profiles/minecraft/" + mcusername)
-    if mc_id_api_call.status_code == 200 and not data:
-        data = mc_id_api_call.json()
-        mc_id = data["id"]
-        cur.execute("UPDATE userbase SET mc_id=? WHERE id=?", (mc_id, userid,))
-        return mcusername
-    elif mc_id_api_call.status_code != 200:
-        return 404
-    elif mc_id_api_call.status_code == 200 and data[0][0] != None:
+    if data and data[0][0] is not None:
         return 405
 
+    # Get Minecraft ID from API
+    mc_id_api_call = requests.get("https://api.mojang.com/users/profiles/minecraft/" + mcusername)
+    uuid_index = "id"
+
+    if mc_id_api_call.status_code != 200 or version == 'bedrock':
+        mc_id_api_call = requests.get("https://mcprofile.io/api/v1/bedrock/gamertag/" + mcusername)
+        if mc_id_api_call.status_code != 200:
+            return 404
+        else:
+            mcusername = "." + mcusername
+            uuid_index = "floodgateuid"
+            version = 'bedrock'
+    else:
+        version = 'java'
+    info = mc_id_api_call.json()
+    mc_id = info[uuid_index]
+    cur.execute("UPDATE userbase SET mc_username=?, mc_id=?, mc_version=? WHERE id=?", (mcusername, mc_id, version, userid,))
+    print(data)
     checkpoint_db()
+    return mcusername
 
 def remove_minecraft_user(userid):
     cur.execute("SELECT mc_username FROM userbase WHERE id=?", (userid,))
     data = cur.fetchall()
     if not data:
-        return False
+        return None
     else:
         tmp = data[0][0]
-        cur.execute("UPDATE userbase SET mc_username=?, mc_id=? WHERE id=?", (None, None, userid,))
+        if read_info(userid, 'version') == 'bedrock':
+            tmp = "." + tmp
+        cur.execute("UPDATE userbase SET mc_username=?, mc_id=?, mc_version=? WHERE id=?", (None, None, None, userid,))
+        checkpoint_db()
         return tmp
